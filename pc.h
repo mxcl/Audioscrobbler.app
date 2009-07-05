de4374e9424f2d5859ca6b6dfe6322ceb106e158
Max Howell <max@methylblue.com>
2009-07-05 14:58:05 +0100

    Make scrobsub a submodule
    Instructions for prebuild added to README

--- pc.h
+++ pc.h
@@ -7,7 +7,10 @@
 #endif
 
 
-#include "../_configure.h"
+// don't use these, make your own!
+#define SCROBSUB_CLIENT_VERSION "2.0.0"
+#define SCROBSUB_API_KEY "c8c7b163b11f92ef2d33ba6cd3c2c3c3"
+#define SCROBSUB_SHARED_SECRET "73582dfc9e556d307aead069af110ab8"
 #define SCROBSUB_CLIENT_ID "ass"
 #define SCROBSUB_NO_RELAY 1
 
