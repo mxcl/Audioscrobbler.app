2d26c2aacc32c3476fc4a181e833c7a16a5b8ecc
Max Howell <max@methylblue.com>
2009-07-07 14:25:44 +0100

    Dashboard widget is now pretty

--- Last.fm.wdgt/main.js
+++ Last.fm.wdgt/main.js
@@ -1,11 +1,13 @@
 function localize(s)
 {
     // lol yeah, we'll do it properly when we can (ie. not just english style)
-	rgx = /(\d+)(\d{3})/;
-	while (rgx.test(s))
-		s = s.replace(rgx, '$1'+','+'$2');
-	return s;
+    rgx = /(\d+)(\d{3})/;
+    while (rgx.test(s))
+        s = s.replace(rgx, '$1'+','+'$2');
+    return s;
 }
+
+var img;
 
 function artist_got_info(xml)
 {
@@ -18,22 +20,37 @@ function artist_got_info(xml)
     document.getElementById('listeners').innerText = localize($('listeners', xml).text()) + ' listeners';
     // this verbose syntax prevents jQuery selecting all the images of the 
     // similar artists as well :(
-    image = $("lfm", xml).children("artist").children("image[size='medium']").text();
-    document.getElementById('image').src = image;
+    var url = $("lfm", xml).children("artist").children("image[size='large']").text();
+    url = url.replace('126', '252');
+
+    img = new Image();
+    img.onload = function() {
+        var h = this.height;
+        var back = document.getElementById('image');
+        back.style.height = parseInt(h)+"px";
+        back.style.backgroundImage='url('+this.src+')';
+        window.resizeTo(286, h+34);
+    }
+    img.src = url;
 }
 
 function set_artist(scpt)
 {
     artist = $.trim( scpt.outputString );
-    document.getElementById('artist').innerText = artist;
+    artiste = document.getElementById('artist');
+    if (artiste.innerText == artist) return;
+
+    artiste.innerText = artist;
+    document.getElementById('listeners').innerText = "";
+    document.getElementById('image').style.backgroundImage='url()';
 
     artist = encodeURIComponent(artist);
 
-	$.ajax( { url      : 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+artist+'&api_key=b25b959554ed76058ac220b7b2e0a026',
-		      method   : 'get',
-		      dataType : 'xml',
-		      success  : artist_got_info
-		    } );
+    $.ajax( { url      : 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist='+artist+'&api_key=b25b959554ed76058ac220b7b2e0a026',
+              method   : 'get',
+              dataType : 'xml',
+              success  : artist_got_info
+            } );
 }
 
 function show()
