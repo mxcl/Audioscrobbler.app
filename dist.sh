c2d0aa24858437689a22ef25e60218e4b27086a4
Max Howell <max@methylblue.com>
2009-07-10 17:17:02 +0100

    Make dist.sh more useful

--- dist.sh
+++ dist.sh
@@ -1,3 +1,5 @@
 rm -r build
-xcodebuild -configuration Release -project Audioscrobbler.xcodeproj && \
-    open build/Release
+xcodebuild -configuration Release -project Audioscrobbler.xcodeproj || exit $?
+open build/Release
+hdiutil create -srcfolder build/Release/Audioscrobbler.app -format UDZO -imagekey zlib-level=9 -scrub Audioscrobbler.dmg
+
