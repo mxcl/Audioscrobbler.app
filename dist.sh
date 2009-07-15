20f36c725a9a450448a140b796a764c1b11cdb02
Max Howell <max@methylblue.com>
2009-07-15 17:50:18 +0100

    Don't exit for certain bits of dist.sh

--- dist.sh
+++ dist.sh
@@ -1,7 +1,7 @@
 set -o errexit
 rm -r build
 xcodebuild -configuration Release -project Audioscrobbler.xcodeproj
-rm -rf /Applications/Audioscrobbler.app
-killall Audioscrobbler
+test -d /Applications/Audioscrobbler.app && rm -rf /Applications/Audioscrobbler.app
+killall Audioscrobbler || true
 mv build/Release/Audioscrobbler.app /Applications
 open /Applications/Audioscrobbler.app
